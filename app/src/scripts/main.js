555323a0a8b9df40064e18ed0a996c31f6390582
Andrew Buttacavoli <Buttacavoli24@gmail.com>
2019-01-25 23:22:39 -0500

    "list is working properly"

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -1,22 +1,28 @@
-
 var app = new Vue({
   el: '#app',
   data: {
-    isActive: false,
     todos: [
-      {text: 'Learn JavaScript'},
-      {text: 'Learn Vue'},
-      {text: "Build Something"}
+      { text: 'Learn JavaScript', done : false },
+      { text: 'Learn Vue', done : false },
+      { text: 'Build something awesome', done : false}
     ]
   },
+  template:
+  `<div class='container'>
+    <input id='input' type='text'>
+    <button @click="addItem">Add</button>
+    <ol>
+      <li @click="todo.done = !todo.done" v-bind:class="[todo.done ? 'complete' : 'active' ]" v-for='todo in todos' >
+        <input type="checkbox">
+        {{todo.text}}
+      </li>
+    </ol>
+  </div>`,
   methods: {
     addItem() {
-      let input = document.querySelector("#input").value
-      app.todos.push({ text: input })
-    },
-    toggleClass(eve) {
-      if(this.isActive) {this.active = false}
-      else {this.isActive = true}
+      let input = document.querySelector('#input');
+      if (input.value.length > 0) { app.todos.push({ text: input.value })
+      }
     }
   }
-})
+});
