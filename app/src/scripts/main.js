bb27a1124ea2bf3aba7f9883d423e9c978d04fb6
Andrew Buttacavoli <Buttacavoli24@gmail.com>
2019-01-28 14:51:56 -0500

    fixed media queries

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -5,8 +5,7 @@ var app = new Vue({
     message: 'Vue To-Do',
     todos: []
   },
-  template:
-  `<div class='container'>
+  template: `<div class='container'>
     <h1> {{ message }} </h1>
     <div class="form-container">
       <input id='input' placeholder="Add New Item.."  @keydown.enter="addItem">
@@ -27,47 +26,42 @@ var app = new Vue({
     addItem() {
       let input = document.getElementById('input');
       if (input.value.length > 0) {
-        app.todos.push({ text: input.value});
+        app.todos.push({
+          text: input.value
+        });
       }
       input.value = "";
-      saveList()
     },
     removeItem(index) {
       app.todos.splice(index, 1);
       const checkbox = document.getElementById('cb');
       checkbox.checked = false;
-      saveList()
     },
     removeList() {
       let itemList = document.querySelector('#itemList');
-      while(itemList.childNodes){
+      while (itemList.childNodes) {
         itemList.removeChild(itemList.childNodes[0]);
-        }
-        save()
-      },
-      mounted() {
-        if (localStorage.getItem('todos')){
-          this.todos.text = JSON.parse(localStorage.getItem('todos'));
-        }
-      },
-      saveList() {
-      let parsed = JSON.stringify(this.todos);
-      localStorage.setItem('todos', parsed);
       }
     },
-    computed: {
-      textEdit() {
-        return {
-          backgroundColor: this.backgroundColor
-        };
+    mounted() {
+      if (localStorage.getItem('todos')) {
+        this.todos.text = JSON.parse(localStorage.getItem('todos'));
       }
-    },
-    watch: {
-      todos: {
-        handler() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true,
+    }
+  },
+  computed: {
+    textEdit() {
+      return {
+        backgroundColor: this.backgroundColor
+      };
+    }
+  },
+  watch: {
+    todos: {
+      handler() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      },
+      deep: true,
     }
   }
 });
